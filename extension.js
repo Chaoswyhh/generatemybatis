0dde7f69835a021bd1ec2f094647599a5e0a2ed3
Chaoswyhh <wyhveneno@163.com>
2020-07-03 16:05:37 +0800

    fix: change command name.

--- extension.js
+++ extension.js
@@ -19,7 +19,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('generatemybatis.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('generatemybatis.generateMybatisInsert', function () {
 		// The code you place here will be executed every time your command is executed
 
 		const editor = vscode.window.activeTextEditor;
